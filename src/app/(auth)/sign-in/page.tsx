6c69bc5a9c25b4db0ba324e86ba78b78901d9080
Zoranspiegel <zoranbow@gmail.com>
2026-04-29 21:21:33 -0500

    auth pages && layout

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+export default function SignInPage() {
+  return (
+    <main className="flex flex-1 items-center justify-center">
+      <h1>SignInPage</h1>
+    </main>
+  );
+}
---
+++ src/app/(auth)/sign-up/sign-up-form.tsx
@@ -0,0 +1,7 @@
+export default function SignUpForm() {
+  return (
+    <div>
+      <h1>sign-up-form</h1>
+    </div>
+  );
+}
